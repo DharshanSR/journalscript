e01b6a7a424742274baa5f83ca24ee5726251725
DharshanSR <dharshanravindran8@gmail.com>
2025-09-08 22:17:33 +0530

    feat: remove SiteHeader component from Home for cleaner layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import { SiteHeader } from "@/components/AppNavbar";
 import { Hero } from "./home/page";
-
-
 
 export default function Home() {
   return (
     <main className="min-h-[100dvh] text-white">
-      <SiteHeader />
       <Hero />
     </main>
   );
